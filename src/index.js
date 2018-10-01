8a3a9a0f74514e09d4950b2d6c54d97cbd8bc273
Anton <anton@adc.sh>
2018-10-01 16:55:30 +0300

    src

--- src/index.js
+++ src/index.js
@@ -1,26 +1,6 @@
-import { debuglog } from 'util'
+import GitHub from '@rqt/github'
 
-const LOG = debuglog('@rqt/test2')
-
-/**
- * A types bug in VS Code.
- * @param {Config} [config] Options for the program.
- * @param {boolean} [config.shouldRun=true] A boolean option. Default `true`.
- * @param {string} config.text A text to return.
- */
-export default async function test2(config = {}) {
-  const {
-    shouldRun = true,
-    text,
-  } = config
-  if (!shouldRun) return
-  LOG('@rqt/test2 called with %s', text)
-  return text
-}
-
-/* documentary types/index.xml */
-/**
- * @typedef {Object} Config Options for the program.
- * @prop {boolean} [shouldRun=true] A boolean option. Default `true`.
- * @prop {string} text A text to return.
- */
+(async () => {
+  const gh = new GitHub('token')
+  gh.repos.create()
+})()
